582d6260632047e81583b9567ff2e07e2ad0a3e2
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2018-09-04 11:07:22 +1000

    update script improve

--- scripts/update_f5pd.sh
+++ scripts/update_f5pd.sh
@@ -1,4 +1,5 @@
-make clean && make
+ssh rock1 "cd ~/f5p && make clean && make" || exit 1
+scp rock1:~/f5p/f5pd ./f5pd || exit 1
 ansible all -m shell -a "service f5pd stop" -K -b
-ansible all -m copy -a "src=f5pd dest=/nanopore/bin/f5pd mode=0755"
+ansible all -m copy -a "src=./f5pd dest=/nanopore/bin/f5pd mode=0755"
 ansible all -m shell -a "service f5pd start" -K -b
