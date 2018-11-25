df337082e7636d40243bd3a982788d09547e34da
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2018-11-25 19:20:37 +1100

    added scripts for ssd nas

---
+++ scripts/run_quian.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+while true; do
+    read -p "This will overwrite stats from the previous run. Do you wish to continue? (y/n) " yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+make clean && make || exit 1
+rm -rf /scratch_nas/scratch/*
+cleanscratch.sh
+test -d data/logs && rm -r data/logs
+mkdir data/logs || exit 1
+
+FOLDER=/scratch_nas/qian/LXBAB136902/LXBAB136902/2-E3-H3/
+FAST5FOLDER=$FOLDER/reads
+PIPELINE_SCRIPT="scripts/fast5_pipeline_quain.sh"
+
+test -d $FOLDER/sam || mkdir $FOLDER/sam || exit 1
+test -d $FOLDER/bam || mkdir $FOLDER/bam || exit 1
+test -d $FOLDER/log2 || mkdir $FOLDER/log2 || exit 1
+test -d $FOLDER/methylation || mkdir $FOLDER/methylation || exit 1
+
+test -d $FAST5FOLDER || exit 1
+ls $FAST5FOLDER/* -lhS | awk '{print $9}' > data/dev.cfg || exit 1;
+
+ansible all -m copy -a "src=$PIPELINE_SCRIPT dest=/nanopore/bin/fast5_pipeline.sh mode=0755" || exit 1
+/usr/bin/time -v ./f5pl data/ip_list.cfg data/dev.cfg 2>&1 | tee log.txt
+ansible all -m shell -a "cd /nanopore/scratch && tar zcvf logs.tgz *.log"
+gather.sh /nanopore/scratch/logs.tgz data/logs/log tgz
+cp log.txt data/logs/
+mv *.cfg data/logs/
+cp $0 data/logs/
+cp $PIPELINE_SCRIPT data/logs/
+cp -r data $FOLDER/f5pmaster
+
---
+++ scripts/run_ssdnas.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+while true; do
+    read -p "This will overwrite stats from the previous run. Do you wish to continue?" yn
+    case $yn in
+        [Yy]* ) make install; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+make clean && make || exit 1
+rm /scratch_nas/scratch/*
+cleanscratch.sh
+test -d data/logs && rm -r data/logs
+mkdir data/logs || exit 1
+
+ls /scratch_nas/778/778-5000ng/778-5000ng_albacore-2.1.3/fast5/* -lhS | awk '{print $9}' > data/dev.cfg;
+
+ansible all -m copy -a "src=scripts/fast5_pipeline_nas.sh dest=/nanopore/bin/fast5_pipeline.sh mode=0755"
+/usr/bin/time -v ./f5pl data/ip_list.cfg data/dev.cfg 2>&1 | tee log.txt
+ansible all -m shell -a "cd /nanopore/scratch && tar zcvf logs.tgz *.log"
+gather.sh /nanopore/scratch/logs.tgz data/logs/log tgz
+cp log.txt data/logs/
+mv *.cfg data/logs/
+cp $0 data/logs/
+cp scripts/fast5_pipeline_nas.sh data/logs/
