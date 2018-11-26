0d35072192ef40cbbb964d55eeaf7015fd03f9b9
Hasindu Gamaarachchi <hasindu2008@gmail.com>
2018-11-26 18:39:47 +1100

    script modifications

--- scripts/methcat.sh
+++ scripts/methcat.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "usage : $0 <meth_tsv_dir> <concat.tsv> <freq.tsv>"
+if [ "$#" -ne 2 ]; then
+    echo "usage : $0 <meth_tsv_dir> <freq.tsv>"
         exit 1
 fi
 
 meth_tsv_dir=$1
-output=$2
-freq=$3
+freq=$2
 
 
-prefix_output=$(basename $output)
-prefix_freq=$(basename $freq)  
+# prefix_output=$(basename $output)
+# prefix_freq=$(basename $freq)  
 
 #concat
-cat $(ls $meth_tsv_dir/*.tsv | head -1) | head -1 > $output
-tail -n +2 -q $meth_tsv_dir/*.tsv >> $output
+# cat $(ls $meth_tsv_dir/*.tsv | head -1) | head -1 > $output
+# tail -n +2 -q $meth_tsv_dir/*.tsv >> $output
 
-scp $output dice:/home/hasgam/scratch/tmp/$prefix_output
+# scp $output dice:/home/hasgam/scratch/tmp/$prefix_output
 
-ssh dice "'" "qsub -cwd -N meth_freq -V -S /bin/bash -pe smp 1 -l mem_requested=64G,tmp_requested=100G,fast_dm=10 ~/scripts/meth_freq.sh $prefix_output $prefix_freq" "'"
+ssh dice "'" "qsub -cwd -N meth_freq -V -S /bin/bash -pe smp 1 -l mem_requested=64G,tmp_requested=100G,fast_dm=10 ~/scripts/meth_freq.sh $meth_tsv_dir $freq" "'"
 
 # NANOPOLISH_FSCRIPT=/home/hasgam/hasindu2008.git/nanopolish/scripts/calculate_methylation_frequency.py
 
